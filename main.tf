36114490d385e32e15fe57fe424018edfca5ab7f
sudarshanasharma <72416797+sudarshanasharma@users.noreply.github.com>
2021-01-11 00:03:37 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 provider "aws" {
-    access_key = "AKIAYDB77H5HQ37R5HGW"
-    secret_key = "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"
+    
     region = "us-west-2"
 }
 resource "aws_instance" "MyInstance" {
